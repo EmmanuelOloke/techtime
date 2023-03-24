02e1ae9b82135f4b15a57dccc5545a7aac07ceae
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-24 23:59:19 +0100

    avatar width adjustments

--- src/components/Avatars.tsx
+++ src/components/Avatars.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { AvatarGroup, Avatar } from '@chakra-ui/react';
+import { AvatarWidth } from '@/types/avatar-width';
 
-const Avatars: React.FC = () => {
+const Avatars: React.FC<AvatarWidth> = ({ width, height }) => {
   return (
     <AvatarGroup size="md">
-      <Avatar src="/assets/avatars/avatar-1.png" width="32px" height="32px" />
-      <Avatar src="/assets/avatars/avatar-2.png" width="32px" height="32px" />
-      <Avatar src="/assets/avatars/avatar-3.png" width="32px" height="32px" />
-      <Avatar src="/assets/avatars/avatar-4.png" width="32px" height="32px" />
-      <Avatar src="/assets/avatars/avatar-5.png" width="32px" height="32px" />
+      <Avatar src="/assets/avatars/avatar-1.png" width={width} height={height} />
+      <Avatar src="/assets/avatars/avatar-2.png" width={width} height={height} />
+      <Avatar src="/assets/avatars/avatar-3.png" width={width} height={height} />
+      <Avatar src="/assets/avatars/avatar-4.png" width={width} height={height} />
+      <Avatar src="/assets/avatars/avatar-5.png" width={width} height={height} />
     </AvatarGroup>
   );
 };
