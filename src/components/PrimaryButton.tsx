1d2951a4d8ac07bc5c3941cf556c1ba787df1c70
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-24 23:57:21 +0100

    type adjustments

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Link } from '@chakra-ui/react';
+import { PrimaryButtonType } from '@/types/primary-button';
+
+const PrimaryButton: React.FC<PrimaryButtonType> = ({ text, link, color, bgColor }) => {
+  return (
+    <Link href={link} textDecoration="none" _hover={{ textDecoration: 'none' }}>
+      <Button
+        color={color}
+        backgroundColor={bgColor}
+        borderRadius="2.2px"
+        padding="8px 16px"
+        width="73px"
+        height="27px"
+        fontSize="8px"
+        _hover={{ opacity: '0.9' }}
+      >
+        {text}
+      </Button>
+    </Link>
+  );
+};
+
+export default PrimaryButton;
