fd7e81cf4bdd0e30d297b6558c5de0103416e009
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-27 13:54:02 +0100

    created type file for course data

---
+++ src/types/course.ts
@@ -0,0 +1,13 @@
+export type Course = {
+    id: number;
+    title: string;
+    courseImageUrl: string;
+    category: string;
+    rating: number;
+    numberOfStudents: number;
+    duration: string;
+    numberOfLessons: number;
+    tutorName: string;
+    tutorAvatarUrl: string;
+    price: number;
+}
