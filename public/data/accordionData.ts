25e17653941fbf7c54932f5a20eb9eefef01c744
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-28 13:03:52 +0100

    created dummy data for the accordion to consume

---
+++ public/data/accordionData.ts
@@ -0,0 +1,38 @@
+export const accordions = [
+  {
+    id: 1,
+    accordionTitle: 'Is there a free trial available?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+  {
+    id: 2,
+    accordionTitle: 'Can I change my plan later?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+  {
+    id: 3,
+    accordionTitle: 'Are the courses lifetime?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+  {
+    id: 4,
+    accordionTitle: 'Do I Get Certified After Taking Courses?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+  {
+    id: 5,
+    accordionTitle: 'How do I reach out to mentors?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+  {
+    id: 6,
+    accordionTitle: 'Do we get job ready after taking courses?',
+    accordionText:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition.',
+  },
+];
