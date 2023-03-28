3591612aed00eb1614fcf7f1c6bf6663218d94fc
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-28 01:40:24 +0100

    created data file for carousel data

---
+++ public/data/carouselData.ts
@@ -0,0 +1,34 @@
+export const feedbacks = [
+  {
+    id: 1,
+    clientStatement:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
+    clientAvatar: '/assets/images/bella-moon.png',
+    clientName: 'Bella Moon',
+    clientPosition: 'Product Designer',
+  },
+  {
+    id: 2,
+    clientStatement:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
+    clientAvatar: '/assets/images/bella-moon.png',
+    clientName: 'Bella Moon',
+    clientPosition: 'Product Designer',
+  },
+  {
+    id: 3,
+    clientStatement:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
+    clientAvatar: '/assets/images/bella-moon.png',
+    clientName: 'Bella Moon',
+    clientPosition: 'Product Designer',
+  },
+  {
+    id: 4,
+    clientStatement:
+      'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
+    clientAvatar: '/assets/images/bella-moon.png',
+    clientName: 'Bella Moon',
+    clientPosition: 'Product Designer',
+  },
+];
