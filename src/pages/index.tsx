eca0d0252b3579d7878ccb0e6458525846eb5aec
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-28 01:36:55 +0100

    rendered built components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head';
-import { Flex, HStack, Image } from '@chakra-ui/react';
+import { Box, Flex, HStack, Image } from '@chakra-ui/react';
 import Hero from '@/components/Hero';
 import Companies from '@/components/Companies';
 import HighQualitySection from '@/components/HighQualitySection';
 import WhyWeAreBest from '@/components/WhyWeAreBest';
 import BrowseCourses from '@/components/BrowseCourses';
+import ClientsFeedback from '@/components/ClientsFeedback';
+import ReadyToConnect from '@/components/ReadyToConnect';
+import FAQ from '@/components/FAQ';
 
 export default function Home() {
   return (
@@ -25,6 +28,12 @@ export default function Home() {
       <WhyWeAreBest />
 
       <BrowseCourses />
+
+      <ClientsFeedback />
+
+      <ReadyToConnect />
+
+      <FAQ />
     </>
   );
 }
