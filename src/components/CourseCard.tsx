abdc63748ff80761ddb009a52b4b0cbc800a6c9c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-29 22:54:10 +0100

    course card responsiveness

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -13,65 +13,242 @@ const CourseCard: React.FC<Props> = ({ courses }) => {
       flexDirection="column"
       gap="16px"
       background="#FFFFFF"
-      width="194px"
-      height="234px"
-      borderRadius="4px"
-      padding="16px 8px"
+      width={{
+        base: '12.125rem',
+        sm: '12.125rem',
+        md: '12.125rem',
+        lg: '23rem',
+        xl: '23rem',
+        '2xl': '23rem',
+      }}
+      height={{
+        base: '14.625rem',
+        sm: '14.625rem',
+        md: '14.625rem',
+        lg: '29.5rem',
+        xl: '29.5rem',
+        '2xl': '29.5rem',
+      }}
+      borderRadius={{
+        base: '0.25rem',
+        sm: '0.25rem',
+        md: '0.25rem',
+        lg: '1rem',
+        xl: '1rem',
+        '2xl': '1rem',
+      }}
+      padding={{
+        base: '1rem 0.5rem',
+        sm: '1rem 0.5rem',
+        md: '1rem 0.5rem',
+        lg: '1rem 1rem',
+        xl: '1rem 1rem',
+        '2xl': '1rem 1rem',
+      }}
       key={course.id}
     >
-      <Flex flexDirection="column" gap="8px">
-        <Flex flexDirection="column" gap="4px">
+      <Flex
+        flexDirection="column"
+        gap={{
+          base: '0.5rem',
+          sm: '0.5rem',
+          md: '0.5rem',
+          lg: '1rem',
+          xl: '1rem',
+          '2xl': '1rem',
+        }}
+      >
+        <Flex
+          flexDirection="column"
+          gap={{
+            base: '0.25rem',
+            sm: '0.25rem',
+            md: '0.25rem',
+            lg: '1rem',
+            xl: '1rem',
+            '2xl': '1rem',
+          }}
+        >
           <Image
             src={course.courseImageUrl}
             alt={course.title}
-            width="176.82px"
-            borderRadius="2px"
+            width={{
+              base: '11rem',
+              sm: '11rem',
+              md: '11rem',
+              lg: '27.438rem',
+              xl: '27.438rem',
+              '2xl': '27.438rem',
+            }}
+            borderRadius={{
+              base: '0.125rem',
+              sm: '0.125rem',
+              md: '0.125rem',
+              lg: '0.25rem',
+              xl: '0.25rem',
+              '2xl': '0.25rem',
+            }}
           />
 
           <Flex justifyContent="space-between">
             <Box
               background="rgba(30, 93, 206, 0.2)"
-              padding="2px 8px"
-              borderRadius="6.4px"
+              padding={{
+                base: '0.125rem 0.5rem',
+                sm: '0.125rem 0.5rem',
+                md: '0.125rem 0.5rem',
+                lg: '0.25rem 1.25rem',
+                xl: '0.25rem 1.25rem',
+                '2xl': '0.25rem 1.25rem',
+              }}
+              borderRadius={{
+                base: '0.4rem',
+                sm: '0.4rem',
+                md: '0.4rem',
+                lg: '1rem',
+                xl: '1rem',
+                '2xl': '1rem',
+              }}
               color="#1E5DCE"
               fontFamily="Satoshi Bold"
               fontWeight="bold"
-              fontSize="4px"
+              fontSize={{
+                base: '0.25rem',
+                sm: '0.25rem',
+                md: '0.25rem',
+                lg: '0.75rem',
+                xl: '0.75rem',
+                '2xl': '0.75rem',
+              }}
             >
               {course.category}
             </Box>
 
             <Flex gap="3.22px" alignItems="center">
               <Flex justifyContent="center" alignItems="center">
-                <Text fontFamily="Satoshi Bold" fontWeight="bold" fontSize="4px" color="#A1A1A1">
+                <Text
+                  fontFamily="Satoshi Bold"
+                  fontWeight="bold"
+                  fontSize={{
+                    base: '0.25rem',
+                    sm: '0.25rem',
+                    md: '0.25rem',
+                    lg: '0.75rem',
+                    xl: '0.75rem',
+                    '2xl': '0.75rem',
+                  }}
+                  color="#A1A1A1"
+                >
                   {course.rating}k
                 </Text>
-                <Image src="/assets/vectors/star.svg" alt="star icon" />
+                <Image
+                  src="/assets/vectors/star.svg"
+                  alt="star icon"
+                  width={{ lg: '0.833rem', xl: '0.833rem', '2xl': '0.833rem' }}
+                />
               </Flex>
 
-              <Text fontFamily="Satoshi Bold" fontWeight="bold" fontSize="4px" color="#A1A1A1">
+              <Text
+                fontFamily="Satoshi Bold"
+                fontWeight="bold"
+                fontSize={{
+                  base: '0.25rem',
+                  sm: '0.25rem',
+                  md: '0.25rem',
+                  lg: '0.75rem',
+                  xl: '0.75rem',
+                  '2xl': '0.75rem',
+                }}
+                color="#A1A1A1"
+              >
                 ({course.numberOfStudents}k+)
               </Text>
             </Flex>
           </Flex>
         </Flex>
 
-        <Flex flexDirection="column" gap="4px">
-          <Text width="155.47px" fontFamily="Clash Display" fontSize="8px" lineHeight="10.8px">
+        <Flex
+          flexDirection="column"
+          gap={{
+            base: '0.25rem',
+            sm: '0.25rem',
+            md: '0.25rem',
+            lg: '1rem',
+            xl: '1rem',
+            '2xl': '1rem',
+          }}
+        >
+          <Text
+            width={{
+              base: '9.7rem',
+              sm: '9.7rem',
+              md: '9.7rem',
+              lg: '20rem',
+              xl: '20rem',
+              '2xl': '20rem',
+            }}
+            fontFamily="Clash Display"
+            fontSize={{
+              base: '0.5rem',
+              sm: '0.5rem',
+              md: '0.5rem',
+              lg: '1.2rem',
+              xl: '1.2rem',
+              '2xl': '1.2rem',
+            }}
+            lineHeight={{
+              base: '0.675rem',
+              sm: '0.675rem',
+              md: '0.675rem',
+              lg: '1.3rem',
+              xl: '1.3rem',
+              '2xl': '1.3rem',
+            }}
+          >
             {course.title}
           </Text>
 
           <Flex justifyContent="space-between">
             <Flex gap="4px" alignItems="center">
-              <Image src="/assets/vectors/schedule.svg" alt="schedule icon" />
-              <Text fontFamily="Satoshi" color="#A1A1A1" fontSize="4px">
+              <Image
+                src="/assets/vectors/schedule.svg"
+                alt="schedule icon"
+                width={{ lg: '1rem', xl: '1rem', '2xl': '1rem' }}
+              />
+              <Text
+                fontFamily="Satoshi"
+                color="#A1A1A1"
+                fontSize={{
+                  base: '0.25rem',
+                  sm: '0.25rem',
+                  md: '0.25rem',
+                  lg: '0.75rem',
+                  xl: '0.75rem',
+                  '2xl': '0.75rem',
+                }}
+              >
                 {course.duration}
               </Text>
             </Flex>
 
             <Flex gap="4px" alignItems="center">
-              <Image src="/assets/vectors/menu-book.svg" alt="menu book icon" />
-              <Text fontFamily="Satoshi" color="#A1A1A1" fontSize="4px">
+              <Image
+                src="/assets/vectors/menu-book.svg"
+                alt="menu book icon"
+                width={{ lg: '1.25rem', xl: '1.25rem', '2xl': '1.25rem' }}
+              />
+              <Text
+                fontFamily="Satoshi"
+                color="#A1A1A1"
+                fontSize={{
+                  base: '0.25rem',
+                  sm: '0.25rem',
+                  md: '0.25rem',
+                  lg: '0.75rem',
+                  xl: '0.75rem',
+                  '2xl': '0.75rem',
+                }}
+              >
                 {course.numberOfLessons} Lessons
               </Text>
             </Flex>
@@ -81,13 +258,46 @@ const CourseCard: React.FC<Props> = ({ courses }) => {
 
       <Flex alignItems="center" justifyContent="space-between">
         <Flex gap="3.22px" alignItems="center">
-          <Avatar src={course.tutorAvatarUrl} name={course.tutorName} size="2xs" />
-          <Text fontFamily="Satoshi Bold" fontWeight="bold" fontSize="6px">
+          <Avatar
+            src={course.tutorAvatarUrl}
+            name={course.tutorName}
+            size={{
+              base: '2xs',
+              sm: '2xs',
+              md: '2xs',
+              lg: 'md',
+              xl: 'md',
+              '2xl': 'md',
+            }}
+          />
+          <Text
+            fontFamily="Satoshi Bold"
+            fontWeight="bold"
+            fontSize={{
+              base: '0.375rem',
+              sm: '0.375rem',
+              md: '0.375rem',
+              lg: '1rem',
+              xl: '1rem',
+              '2xl': '1rem',
+            }}
+          >
             {course.tutorName}
           </Text>
         </Flex>
 
-        <Text fontFamily="Clash Display" fontSize="8px" color="#004DB3">
+        <Text
+          fontFamily="Clash Display"
+          fontSize={{
+            base: '0.5rem',
+            sm: '0.5rem',
+            md: '0.5rem',
+            lg: '1.25rem',
+            xl: '1.25rem',
+            '2xl': '1.25rem',
+          }}
+          color="#004DB3"
+        >
           ${course.price.toFixed(2)}
         </Text>
       </Flex>
