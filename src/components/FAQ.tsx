dd23988f69096d89f899f9690a15719f7cfa108d
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-30 01:16:43 +0100

    FAQ responsiveness

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -4,19 +4,62 @@ import FAQAccordion from './FAQAccordion';
 
 const FAQ = () => {
   return (
-    <VStack height="572px" gap="8px" position="relative">
+    <VStack
+      height={{
+        base: '35.75rem',
+        sm: '35.75rem',
+        md: '35.75rem',
+        lg: '45.938rem',
+        xl: '45.938rem',
+        '2xl': '45.938rem',
+      }}
+      gap="8px"
+      position="relative"
+    >
       <VStack marginTop="40px">
-        <Text fontFamily="Clash Display" fontSize="24px" textTransform="capitalize">
+        <Text
+          fontFamily="Clash Display"
+          fontSize={{
+            base: '1.5rem',
+            sm: '1.5rem',
+            md: '1.5rem',
+            lg: '3.5rem',
+            xl: '3.5rem',
+            '2xl': '3.5rem',
+          }}
+          textTransform="capitalize"
+        >
           frequently asked questions
         </Text>
 
         <Text
           fontFamily="Satoshi"
-          fontSize="8px"
+          fontSize={{
+            base: '0.5rem',
+            sm: '0.5rem',
+            md: '0.5rem',
+            lg: '1rem',
+            xl: '1rem',
+            '2xl': '1rem',
+          }}
           color="#A1A1A1"
           textAlign="center"
-          lineHeight="16px"
-          width="284px"
+          lineHeight={{
+            base: '1rem',
+            sm: '1rem',
+            md: '1rem',
+            lg: '1.75rem',
+            xl: '1.75rem',
+            '2xl': '1.75rem',
+          }}
+          width={{
+            base: '17.75rem',
+            sm: '17.75rem',
+            md: '17.75rem',
+            lg: '44.438rem',
+            xl: '44.438rem',
+            '2xl': '44.438rem',
+          }}
           textTransform="capitalize"
         >
           high-defination video is video of higher resolution and quality than standard definition.
