ad87e68c6c12b29fda6b1c313f1c91e158ebf040
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-30 04:15:51 +0100

    avatar imports and name edits

--- public/data/carouselData.ts
+++ public/data/carouselData.ts
@@ -3,8 +3,8 @@ export const feedbacks = [
     id: 1,
     clientStatement:
       'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
-    clientAvatar: '/assets/images/bella-moon.png',
-    clientName: 'Bella Moon',
+    clientAvatar: '/assets/images/jessica-duke.png',
+    clientName: 'Jessica Duke',
     clientPosition: 'Product Designer',
   },
   {
@@ -19,16 +19,16 @@ export const feedbacks = [
     id: 3,
     clientStatement:
       'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
-    clientAvatar: '/assets/images/bella-moon.png',
-    clientName: 'Bella Moon',
+    clientAvatar: '/assets/images/samuel-fortune.png',
+    clientName: 'Samuel Fortune',
     clientPosition: 'Product Designer',
   },
   {
     id: 4,
     clientStatement:
       'High-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. build your future with our quality education. the best and largest all-in-one online tutoring platform in the worldd definition. high-defination video is video of higher resolution and quality than standard definition. build your future withour quality education. the best and largest all-in-one online tutoring platform in the world',
-    clientAvatar: '/assets/images/bella-moon.png',
-    clientName: 'Bella Moon',
+    clientAvatar: '/assets/images/bryce-jason.png',
+    clientName: 'Bryce Jason',
     clientPosition: 'Product Designer',
   },
 ];
